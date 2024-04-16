b0f1d1560823cc1d3681e6f5f586cb5ce53d3eb8
tony <tony@trustless.computer>
2024-04-16 14:51:48 +0700

    init

---
+++ data/meta.ts
@@ -0,0 +1,18 @@
+export const portal = {
+  title: "RuneChain Portal | View balances, transfer and bridge tokens",
+  description:
+    "RuneChain Portal allows you to view your balances, transfer tokens and bridge assets between RuneChain and BVM",
+  previewImg: {
+    src: "https://storage.googleapis.com/tc-cdn-prod/nbc/images/Rune%20%20Chain.jpg",
+    alt: "RuneChain Portal",
+  },
+};
+export const bridge = {
+  title: "RuneChain Bridge | Transfer funds between RuneChain and BVM",
+  description:
+    "With the RuneChain Bridge you can easily deposit tokens to RuneChain. Enjoy faster, cheaper and more efficient transactions with the future proof zkEVM scaling Bitcoin's security and values.",
+  previewImg: {
+    src: "https://storage.googleapis.com/tc-cdn-prod/nbc/images/Rune%20%20Chain.jpg",
+    alt: "RuneChain Bridge",
+  },
+};
