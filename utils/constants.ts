4442e312d7375b5c298c534b8ae80e292b5e5889
tony <tony@trustless.computer>
2024-06-07 11:50:04 +0700

    source RuneChain

---
+++ utils/constants.ts
@@ -0,0 +1,10 @@
+import type { Token } from "@/types";
+
+export const ETH_TOKEN: Token = {
+  address: "0x000000000000000000000000000000000000800A",
+  l1Address: "0x0000000000000000000000000000000000000000",
+  symbol: "BVM",
+  name: "BVM",
+  decimals: 18,
+  iconUrl: "/img/bvm_token_ic.svg",
+};
